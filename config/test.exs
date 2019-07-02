92e80bb7ecd1dffcb7b1cafbac2fb5a85802bc1d
Dave J <david@dvj.me.uk>
2019-07-02 22:40:01 +0100

    Resolve DB connection failures in test env
    Update test env configuration to read its configuration from Heroku
    environment variables, just as we do in production.

--- config/test.exs
+++ config/test.exs
@@ -1,18 +1,26 @@
 use Mix.Config
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
-config :furagent, FuragentWeb.Endpoint,
-  http: [port: 4002],
-  server: false
+database_url =
+  System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
 
-# Print only warnings and errors during test
-config :logger, level: :warn
-
-# Configure your database
 config :furagent, Furagent.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "furagent_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  ssl: true,
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+config :furagent, FuragentWeb.Endpoint,
+  http: [:inet6, port: System.get_env("PORT") || 4000],
+  url: [scheme: "https", host: "furagent-sandbox.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json"
